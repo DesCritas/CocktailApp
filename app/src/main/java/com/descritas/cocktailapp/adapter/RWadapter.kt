ed423b3e2105d3f6324954959c10fc928ca5ae59
michaelbel <michael-bel@outlook.com>
2023-03-12 13:59:37 +0300

    sixth version fix

--- app/src/main/java/com/descritas/cocktailapp/adapter/RWadapter.kt
+++ app/src/main/java/com/descritas/cocktailapp/adapter/RWadapter.kt
@@ -1,6 +1,5 @@
 package com.descritas.cocktailapp.adapter
 
-
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.DiffUtil
@@ -9,10 +8,9 @@ import androidx.recyclerview.widget.RecyclerView
 import com.descritas.cocktailapp.databinding.CocktailCardBinding
 import com.descritas.cocktailapp.databinding.IngredientItemBinding
 import com.descritas.cocktailapp.dto.Data
-import com.descritas.cocktailapp.dto.Ingredient
 import com.descritas.cocktailapp.load
 
-class RWAdapter(var list: ArrayList<Data>): ListAdapter<Ingredient,RecyclerView.ViewHolder>(DiffCallback()) {
+class RWAdapter: ListAdapter<Data, RecyclerView.ViewHolder>(DiffCallback()) {
 
     companion object {
         const val VIEW_TYPE_ONE = 1
@@ -28,24 +26,23 @@ class RWAdapter(var list: ArrayList<Data>): ListAdapter<Ingredient,RecyclerView.
         return MyListViewHolder(bindingIngr)
     }
 
-    override fun getItemCount(): Int {
-        return list.size
-    }
     override fun getItemViewType(position: Int): Int {
-        return list[position].viewType
+        return getItem(position).viewType
     }
+
     override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
-        if (list[position].viewType == VIEW_TYPE_ONE) {
+        if (getItem(position).viewType == VIEW_TYPE_ONE) {
             (holder as MyCardViewHolder).bind(position)
         } else {
             (holder as MyListViewHolder).bind(position)
         }
     }
+
     private inner class MyCardViewHolder(
         private val binding: CocktailCardBinding
     ): RecyclerView.ViewHolder(binding.root){
         fun bind(position: Int){
-            val recyclerViewModel = list[position]
+            val recyclerViewModel = getItem(position)
             val card = recyclerViewModel.card
             binding.apply {
                 cocktailId.text = card.id.toString()
@@ -65,21 +62,23 @@ class RWAdapter(var list: ArrayList<Data>): ListAdapter<Ingredient,RecyclerView.
     ): RecyclerView.ViewHolder(binding.root) {
 
         fun bind(position: Int){
-            val recyclerViewModel1 = list[position]
+            val recyclerViewModel1: Data = getItem(position)
             val ingredients = recyclerViewModel1.ingredients
-            binding.apply {
-                ingredient.text = ingredients?.ingredient
-                measure.text = ingredients?.measure
+            if (ingredients != null) {
+                binding.apply {
+                    ingredient.text = ingredients.ingredient
+                    measure.text = ingredients.measure
+                }
             }
         }
     }
 }
-class DiffCallback : DiffUtil.ItemCallback<Ingredient>() {
-    override fun areItemsTheSame(oldItem: Ingredient, newItem: Ingredient): Boolean {
-        return oldItem.ingredient == newItem.ingredient && oldItem.measure == newItem.measure
+class DiffCallback : DiffUtil.ItemCallback<Data>() {
+    override fun areItemsTheSame(oldItem: Data, newItem: Data): Boolean {
+        return oldItem.viewType == newItem.viewType && oldItem.card == newItem.card && oldItem.ingredients == newItem.ingredients
     }
 
-    override fun areContentsTheSame(oldItem: Ingredient, newItem: Ingredient): Boolean {
+    override fun areContentsTheSame(oldItem: Data, newItem: Data): Boolean {
         return oldItem == newItem
     }
 }
